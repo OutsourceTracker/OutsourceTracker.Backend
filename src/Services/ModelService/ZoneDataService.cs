635d08504baaba073814cf67dd8a52a9f21f4bfb
Austin Vandersluis <austin@vandersluis.club>
2026-03-18 22:41:31 -0700

    Add: ZoneDataService for managing Zone entities

---
+++ src/Services/ModelService/ZoneDataService.cs
@@ -0,0 +1,27 @@
+﻿using OutsourceTracker.Data;
+using OutsourceTracker.Geolocation;
+using OutsourceTracker.Models.Zones;
+
+namespace OutsourceTracker.Services.ModelService;
+
+internal class ZoneDataService : DynamicDataService<Zone>
+{
+    public ZoneDataService(IServiceProvider services) : base(services)
+    {
+    }
+
+    protected override Task NormalizeModel(Zone model, CancellationToken cancellationToken)
+    {
+        model.ShortCode = model.ShortCode.ToUpperInvariant().Trim();
+        model.FullName = model.FullName.Trim();
+        return Task.CompletedTask;
+    }
+
+    protected override Task OnModelCreated(Zone model, CancellationToken cancellationToken)
+    {
+        model.DockPoints ??= new List<Vector2>();
+        model.EntryPoints ??= new List<Vector2>();
+        model.ExitPoints ??= new List<Vector2>();
+        return base.OnModelCreated(model, cancellationToken);
+    }
+}
