d5f0475a5769e8f3664d2eb515ff29ee32a123af
Austin Vandersluis <austin@vandersluis.club>
2026-03-19 01:20:40 -0700

    Add: EmailService for sending transactional emails

---
+++ src/Services/EmailService.cs
@@ -0,0 +1,35 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace OutsourceTracker.Services;
+
+public class EmailService
+{
+    private readonly ISendGridClient _client;
+    private readonly IConfiguration _config;
+
+    public EmailService(ISendGridClient client, IConfiguration config)
+    {
+        _config = config.GetRequiredSection("SendGrid");
+        _client = client;
+    }
+
+    public async Task SendEmailAsync(string toEmail, string subject, string htmlContent)
+    {
+        var msg = new SendGridMessage()
+        {
+            From = new EmailAddress(_config["FromEmail"], _config["FromName"]),
+            Subject = subject,
+            HtmlContent = htmlContent
+        };
+        msg.AddTo(new EmailAddress(toEmail));
+
+        var response = await _client.SendEmailAsync(msg);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            var body = await response.Body.ReadAsStringAsync();
+            throw new Exception($"Email sending failed: {response.StatusCode} - {body}");
+        }
+    }
+}
