0f6a980dd48e28a6b3a69bc2fb37ff681a07d3c1
Austin Vandersluis <austin@vandersluis.club>
2026-03-19 01:20:39 -0700

    Add: Custom ApplicationUser entity for Identity

---
+++ src/Authentication/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OutsourceTracker.Authentication;
+
+public class ApplicationUser : IdentityUser<Guid>
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string FullName { get; set; }
+
+    public string AlphaCode { get; set; }
+
+    public string WorkdayId { get; set; }
+}
