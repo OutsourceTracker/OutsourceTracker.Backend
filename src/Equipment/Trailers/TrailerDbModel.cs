5f01c4327d2f107118961ab9e6575a1b8a404393
Austin Vandersluis <austin@vandersluis.club>
2026-05-03 20:17:42 -0700

    Refactor: Migrate Trailer services to AppDataModelService

--- src/Equipment/Trailers/TrailerDbModel.cs
+++ src/Equipment/Trailers/TrailerDbModel.cs
@@ -1,11 +1,22 @@
-﻿using OutsourceTracker.Models.Accounts;
+﻿using Microsoft.EntityFrameworkCore;
+using OutsourceTracker.Authentication;
+using OutsourceTracker.BusinessUnit.Accounts;
 using OutsourceTracker.Models.Zones;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OutsourceTracker.Equipment.Trailers;
 
 public class TrailerDbModel : TrailerModel
 {
-    public Account? Account { get; set; }
+    [ForeignKey(nameof(AccountId))]
+    [DeleteBehavior(DeleteBehavior.SetNull)]
+    public AccountDbModel? Account { get; set; }
 
+    [ForeignKey(nameof(ZoneId))]
+    [DeleteBehavior(DeleteBehavior.SetNull)]
     public Zone? Zone { get; set; }
+
+    [ForeignKey(nameof(LocatedById))]
+    [DeleteBehavior(DeleteBehavior.SetNull)]
+    public ApplicationUser? LocatedBy { get; set; }
 }
---
+++ src/Equipment/Trailers/TrailerService.cs
@@ -0,0 +1,19 @@
+﻿using OutsourceTracker.Data;
+
+namespace OutsourceTracker.Equipment.Trailers;
+
+internal class TrailerService : AppDataModelService<TrailerDbModel>
+{
+    public TrailerService(IServiceProvider services) : base(services)
+    {
+    }
+
+    protected override ValueTask OnModelCreated(TrailerDbModel model, CancellationToken cancellationToken = default)
+    {
+        model.Prefix = model.Prefix.Trim().ToUpperInvariant();
+        model.Name = model.Name.Trim().ToUpperInvariant();
+        model.FullName = $"{model.Prefix} {model.Name}";
+        model.CreatedOn = DateTimeOffset.UtcNow;
+        return base.OnModelCreated(model, cancellationToken);
+    }
+}
